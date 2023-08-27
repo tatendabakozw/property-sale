9aeffc03492909cea6b057f42bf09c3725ab8e33
tatendabakozw <tatendabakozw@gmail.com>
2023-08-27 19:58:59 +0200

    featrure: added location pictures on landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
 import GeneralLayout from "@/layouts/GeneralLayout";
 import HeroSection from "@/components/page-sections/home/HeroSection";
 import PropertyItem from "@/components/property-item/PropertyItem";
+import harare from "@/public/harare.jpg";
+import masvingo from "@/public/masvingo.jpg";
+import bulawayo from "@/public/bulawayo.jpg";
+import mutare from "@/public/mutare.jpeg";
 
 export default function Home() {
   return (
@@ -31,11 +35,10 @@ export default function Home() {
               Look for listings in your favorite cities
             </p>
             <div className="grid md:grid-cols-3 grid-cols-1 gap-8 pt-16">
-              <div className="col-span-1 md:h-96 h-80 rounded-lg bg-blue-700"></div>
-              <div className="md:col-span-2 col-span-1 md:h-96 h-40 rounded-lg bg-blue-700"></div>
-              <div className="md:col-span-2 col-span-1 md:h-96 h-40 rounded-lg bg-blue-700"></div>
-              <div className="col-span-1 md:h-96 h-80 rounded-lg bg-blue-700"></div>
-
+              <LocationItem span="col-span-1" image={harare} location="Harare" properties="60" />
+              <LocationItem span="md:col-span-2 col-span-1" image={bulawayo} location="Bulawayo" properties="12" />
+              <LocationItem span="md:col-span-2 col-span-1" image={masvingo} location="Masvingo" properties="13" />
+              <LocationItem span="col-span-1" image={mutare} location="Mutare" properties="30" />
             </div>
           </div>
         </div>
@@ -43,3 +46,34 @@ export default function Home() {
     </GeneralLayout>
   );
 }
+
+interface ItemLocationProps {
+  span: string;
+  image: any;
+  location: string;
+  properties: string;
+}
+
+const LocationItem = ({
+  span,
+  image,
+  location,
+  properties,
+}: ItemLocationProps) => {
+  return (
+    <div
+      className={`${span} md:h-96 h-80 rounded-lg p-6 relative flex flex-col items-center overflow-hidden bg-no-repeat cursor-pointer`}
+      style={{
+        backgroundImage: `url(${image.src})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        width: "100%",
+      }}
+    >
+      <div className="overlay  absolute  top-0 right-0 left-0 bottom-0 bg-black z-0 opacity-40" />
+      <div className="flex-1"></div>
+      <p className="text-white font-semibold text-lg z-10">{location}</p>
+      <p className="text-white z-10">{properties} Properties</p>
+    </div>
+  );
+};
