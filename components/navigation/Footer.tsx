0717c9ad764e494c3b491ca78dbb691d9609ebc0
tatendabakozw <tatendabakozw@gmail.com>
2023-08-28 12:30:55 +0200

    feature: added new

--- components/navigation/Footer.tsx
+++ components/navigation/Footer.tsx
@@ -1,11 +1,18 @@
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
 import React from "react";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaPinterest,
+} from "react-icons/fa";
 
 type Props = {};
 
 function Footer({}: Props) {
   return (
     <div className="bg-slate-800 flex flex-col py-16">
-      <div className="max-w-7xl w-full mx-auto grid grid-cols-4 gap-8">
+      <div className="max-w-7xl w-full mx-auto grid md:grid-cols-4 grid-cols-1 gap-8 px-2">
         <div className="col-span-1 flex flex-col space-y-4">
           <p className="text-lg font-semibold text-white">About Site</p>
           <p className="text-slate-500 text-sm">
@@ -19,7 +26,6 @@ function Footer({}: Props) {
           <p className="text-slate-500 text-sm">Terms & Conditions</p>
           <p className="text-slate-500 text-sm">Users Guide</p>
           <p className="text-slate-500 text-sm">Contact</p>
-
         </div>
         <div className="col-span-1 flex flex-col space-y-4">
           <p className="text-lg font-semibold text-white">Contact Us</p>
@@ -30,10 +36,19 @@ function Footer({}: Props) {
         </div>
         <div className="col-span-1 flex flex-col space-y-4">
           <p className="text-lg font-semibold text-white">Follow us</p>
-          <p className="text-slate-500 text-sm">info@site.com</p>
-          <p className="text-slate-500 text-sm">15002 Zengeza 3 Ext</p>
-          <p className="text-slate-500 text-sm">Chitungwiza, Harare</p>
-          <p className="text-slate-500 text-sm">+263771445411</p>
+          <div className="text-slate-500 text-sm flex flex-row items-center space-x-4">
+            <FaFacebookF />
+            <FaTwitter />
+            <FaInstagram />
+            <FaPinterest />
+          </div>
+          <p className="text-lg font-semibold text-white">Subscribe</p>
+          <div className="flex flex-row items-center space-x-2 w-16">
+            <input type="text" className="p-3 rounded-full bg-slate-700 w-56" placeholder="Your Email" />
+            <div className="rounded-full bg-slate-700 p-4">
+              <ChevronRightIcon height={16} width={16} className="text-white"/>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,17 +13,17 @@ export default function Home() {
         <PropertiesSection />
         <LocationSection />
         <WhyUsSection />
-        <div className="flex bg-primary-original py-16 text-white">
-          <div className="max-w-7xl flex flex-row items-center justify-between mx-auto w-full">
-            <div className="flex flex-col space-y-2 ">
-              <p className="text-3xl font-semibold">
-                Become a Real Estate Agent
+        <div className="flex bg-primary-original py-16 text-white px-2">
+          <div className="max-w-7xl flex md:flex-row flex-col items-center justify-between mx-auto w-full">
+            <div className="flex flex-col md:items-start items-center space-y-2 ">
+              <p className="text-3xl md:text-start text-center font-semibold">
+              Find Your Dream Home Now
               </p>
-              <p className="text-sm">
+              <p className="text-sm md:text-start text-center">
                 We only work with the best companies around the globe
               </p>
             </div>
-            <div className="bg-primary-light p-4 rounded-lg text-white">
+            <div className="bg-primary-light p-4 rounded-lg md:mt-0 mt-8 text-white">
               Explore Properties
             </div>
           </div>
